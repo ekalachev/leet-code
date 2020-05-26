12309d0360fdeb61e2a3e995d748cc13488443ab
Eldar Kalachev <Eldar.Kalachev@thomsonreuters.com>
2020-05-26 12:45:12 +0300

    Remove Duplicates from Sorted Array

---
+++ src/com/company/array/remove_duplicates_from_sorted_array/Solution.java
@@ -0,0 +1,35 @@
+package com.company.array.remove_duplicates_from_sorted_array;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int currentPosition = 1;
+        int prev = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (prev != nums[i]) {
+                nums[currentPosition] = nums[i];
+
+                currentPosition++;
+                prev = nums[i];
+            }
+        }
+
+        return currentPosition;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        int len = solution.removeDuplicates(nums);
+
+        for (int i = 0; i < len; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
