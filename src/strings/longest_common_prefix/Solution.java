35038dbbad5e91c56773edd1ebcfb60f892a5183
ekalachev <rusgunter@gmail.com>
2020-06-01 15:04:09 +0300

    Longest Common Prefix

--- src/strings/longest_common_prefix/Solution.java
+++ src/strings/longest_common_prefix/Solution.java
@@ -3,6 +3,19 @@ package strings.longest_common_prefix;
 public class Solution {
     // time complexity: O(S) where S is the sum of all characters in all strings, space complexity: O(1)
     public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i++) {
+            while(!strs[i].startsWith(prefix))
+                prefix = prefix.substring(0, prefix.length() - 1);
+        }
+
+        return prefix;
+    }
+
+    // time complexity: O(S) where S is the sum of all characters in all strings, space complexity: O(1)
+    public String longestCommonPrefix2(String[] strs) {
         if (strs.length == 0) return "";
 
         String prefix = strs[0];
@@ -14,18 +27,6 @@ public class Solution {
         }
 
         return prefix;
-    }
-
-    // time complexity: O(S) where S is the sum of all characters in all strings, space complexity: O(1)
-    public String longestCommonPrefix2(String[] strs) {
-        if (strs.length == 0) return "";
-        String res = strs[0];
-        for (String str : strs) {
-            while (!str.startsWith(res)) {
-                res = res.substring(0, res.length() - 1);
-            }
-        }
-        return res;
     }
 
     // time complexity: O(S) where S is the sum of all characters in all strings, space complexity: O(1)
