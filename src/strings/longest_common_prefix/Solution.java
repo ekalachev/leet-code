da5f01c76af2d120105582da5b06fb9634ac21ef
ekalachev <rusgunter@gmail.com>
2020-06-01 13:09:15 +0300

    Longest Common Prefix

--- src/strings/longest_common_prefix/Solution.java
+++ src/strings/longest_common_prefix/Solution.java
@@ -16,12 +16,50 @@ public class Solution {
         return prefix;
     }
 
+    // time complexity: O(S) where S is the sum of all characters in all strings, space complexity: O(1)
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs.length == 0) return "";
+        String res = strs[0];
+        for (String str : strs) {
+            while (!str.startsWith(res)) {
+                res = res.substring(0, res.length() - 1);
+            }
+        }
+        return res;
+    }
+
+    // time complexity: O(S) where S is the sum of all characters in all strings, space complexity: O(1)
+    public String longestCommonPrefix3(String[] strs) {
+        if(strs.length == 0 || strs[0].length() == 0) return "";
+
+        int index = 0;
+
+        do {
+            char c = strs[0].charAt(index);
+            for(int i = 1; i < strs.length; i++) {
+                if(strs[i].length() <= index || c != strs[i].charAt(index))
+                    return strs[0].substring(0, index);
+            }
+
+            index++;
+        }
+        while(strs[0].length() > index);
+
+        return strs[0].substring(0, index);
+    }
+
     public static void main(String[] args) {
         String[] strs = new String[]{"flower", "flow", "flight"};
 
         Solution solution = new Solution();
-        String prefix = solution.longestCommonPrefix(strs);
 
+        String prefix = solution.longestCommonPrefix(strs);
+        System.out.println(prefix);
+
+        prefix = solution.longestCommonPrefix2(strs);
+        System.out.println(prefix);
+
+        prefix = solution.longestCommonPrefix3(strs);
         System.out.println(prefix);
     }
 }
