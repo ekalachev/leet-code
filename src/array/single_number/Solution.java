455b8e1bd45c07d74f44c4a39bab2a9d4531682f
ekalachev <rusgunter@gmail.com>
2020-06-12 19:47:37 +0300

    Single Number (improvement)

--- src/array/single_number/Solution.java
+++ src/array/single_number/Solution.java
@@ -1,41 +1,25 @@
 package array.single_number;
 
-import java.util.HashSet;
-
 public class Solution {
-    // time complexity: O(n), space complexity: O(n)
-    public int singleNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int num : nums) {
-            if (set.contains(num))
-                set.remove(num);
-            else
-                set.add(num);
-        }
-
-        return set.stream().findFirst().get();
-    }
-
+    /*
+        If we take XOR of zero and some bit, it will return that bit a ^ 0 = a
+        If we take XOR of two same bits, it will return 0, a ^ a = 0
+     */
     // time complexity: O(n), space complexity: O(1)
-    public int singleNumber2(int[] nums) {
+    public int singleNumber(int[] nums) {
         int result = 0;
-
-        for (int num : nums) {
-            result ^= num;
+        for(int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
         }
-
         return result;
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
         int[] nums = new int[]{2, 2, 1};
 
+        Solution solution = new Solution();
         int result = solution.singleNumber(nums);
-        System.out.println(result);
 
-        int result2 = solution.singleNumber(nums);
-        System.out.println(result2);
+        System.out.println(result);
     }
 }
