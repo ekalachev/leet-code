2218a7b0781d6c9f84fe89412b9d5490f09630ff
ekalachev <rusgunter@gmail.com>
2020-06-12 18:51:16 +0300

    Best Time to Buy and Sell Stock II (improvement)

--- src/array/best_time_to_buy_and_sell_stock_2/Solution.java
+++ src/array/best_time_to_buy_and_sell_stock_2/Solution.java
@@ -3,17 +3,10 @@ package array.best_time_to_buy_and_sell_stock_2;
 public class Solution {
     // time complexity: O(n), space complexity: O(1)
     public int maxProfit(int[] prices) {
-        if(prices == null || prices.length < 1) {
-            return 0;
-        }
-
         int max = 0;
-
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                max += prices[i] - prices[i - 1];
-            }
-        }
+        for(int i = 0; i < prices.length - 1; i++)
+            if(prices[i] < prices[i + 1])
+                max += prices[i + 1] - prices[i];
 
         return max;
     }
