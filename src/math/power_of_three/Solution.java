de06a821191f259fa57acd8a5bda47a5dc57d385
Eldar Kalachev <rusgunter@gmail.com>
2020-06-24 22:41:43 +0300

    Power of Three

---
+++ src/math/power_of_three/Solution.java
@@ -0,0 +1,18 @@
+package math.power_of_three;
+
+public class Solution {
+    // n is a power of three if and only if i is an integer. In Java, we check if a number is an integer by taking the decimal part (using % 1) and checking if it is 0.
+    public boolean isPowerOfThree(int n) {
+        if(n < 1) return false;
+        
+        return (Math.log10(n) / Math.log10(3)) % 1 == 0;
+    }
+
+    public static void main(final String[] args) {
+
+        Solution solution = new Solution();
+        boolean result = solution.isPowerOfThree(3);
+
+        System.out.println("Result is: " + result);
+    }
+}
